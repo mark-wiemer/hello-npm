36f5c817b64462fafccc86306a548a3516aebe5f
Mark Wiemer <markwiemer@outlook.com>
2022-10-15 16:38:34 -0700

    Initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Hello Vitest
+
+The simplest project demonstrating the features of Vitest
+
+## Installation
+
+`npm i && npm test`
---
+++ src/main.spec.ts
@@ -0,0 +1,7 @@
+import { describe, it, expect } from 'vitest';
+
+import { add } from './main';
+
+describe('add', () => {
+    it('1 + 1 = 2', () => expect(add(1, 1)).toBe(2));
+})
