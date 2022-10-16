0b0c3e8e3d56da7e76e4416ea47e71aca56d6180
Mark Wiemer <markwiemer@outlook.com>
2022-10-15 20:27:53 -0700

    Add Husky, lint-staged, and TSC

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,30 @@
+import { subtract } from './utils'; // tsconfig.json#baseUrl :)
+
 export const add = (a: number, b: number) => a + b;
 
-function foo(bar) {
+function foo(bar: number) {
+    subtract(bar, 1);
     if (bar === 2) {
         // use double-equals to test ESLint
         console.log('wowow');
     }
 }
 
-foo(1); // if the function isn't called, ESLint throws an error
+if (Math.random() < -1) {
+    switch (Math.random()) {
+        case 1:
+            console.log('wow');
+            break;
+        case 2:
+            console.log('oh my');
+    }
+
+    interface Indexed {
+        known: string;
+        [key: string]: string;
+    }
+
+    const x: Indexed = { known: 'wow', unknown: 'uh oh' };
+    foo(1); // if the function isn't called, ESLint throws an error
+    console.log(x['unknown']);
+}
