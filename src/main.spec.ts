7fbe0e932009630103cbce3186eb28273829dfe6
Mark Wiemer <markwiemer@outlook.com>
2022-10-15 19:04:55 -0700

    Add ESLint

--- src/main.spec.ts
+++ src/main.spec.ts
@@ -1,7 +1,7 @@
-import { describe, it, expect } from 'vitest';
+import { describe, it, expect } from "vitest";
 
-import { add } from './main';
+import { add } from "./main";
 
-describe('add', () => {
-    it('1 + 1 = 2', () => expect(add(1, 1)).toBe(2));
-})
+describe("add", () => {
+  it("1 + 1 = 2", () => expect(add(1, 1)).toBe(2));
+});
--- src/main.ts
+++ src/main.ts
@@ -2,4 +2,10 @@ export const add = (a: number, b: number) => a + b;
 
 function foo(bar) {
   console.log("potato");
+  if (bar === 2) {
+    // use double-equals to test ES Lint
+    console.log("wowow");
+  }
 }
+
+foo(1);
